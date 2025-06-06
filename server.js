529a98178e1becebed321c39be201ea82b5258c9
cauacozz11 <cauacozz@gmail.com>
2025-06-06 15:27:46 -0300

    comeco do trabalho

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let time = [
+    { id: 1, jogador: "Fabin", numero: 11 },
+    { id: 2, jogador: "Jeferson", numero: 9},
+    { id: 3, jogador: "Marcelin", numero: 1},
+    { id: 4, jogador: "Ximbinha", numero: 21},
+]
