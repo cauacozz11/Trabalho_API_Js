f68c9e33cfef0971e8e9f596fcfe7e5f0c825e66
cauacozz11 <cauacozz@mail.com>
2025-06-12 21:44:13 -0300

    POST caceta preta e mole

--- server.js
+++ server.js
@@ -13,6 +13,18 @@ let jogador = [
 ];
  
 
+
+/* O Array foi criado para guardar as informações do clube*/
+let clube = [
+    { id: 1, time: "Ibis" , presidente: "Ozir Ramos Junior" , estadio: "Estádio Municipal Ademir Cunha"},
+    { id: 2, time: "Ponte preta" , presidente: "Marco Antônio" , estadio: "Estádio Moisés Lucarelli"},
+    { id: 3, time: "Arsenal" , presidente: "Stan Kroenke" , estadio: "Emirates Stadium"},
+    { id: 4, time: "Foraleza" ,  presidente: "José Rolim" , estadio: "Arena Castelão"},
+    { id: 5, time: "Maranhão Atlético clube" , presidente: "Carlos Eduardo" , estadio: "Estádio Governador São Castelo"},
+];
+
+
+
 /* Aplicação de GET específico por ID do objeto*/
 app.get ("/jogador/:id", (req, res) => {
     const id = parseInt(req.params.id);
@@ -27,10 +39,12 @@ app.get ("/jogador/:id", (req, res) => {
 });
 
 
+
 /* Aplicação do GET geral, revelano o array inteiro */
 app.get ("/jogador", (req, res) => {
     res.json(jogador);
 });
+
 
 
 /* Aplicação do PUT, onde o usuário pode atualizar alguma informação do array */
@@ -48,6 +62,7 @@ app.put("/jogador/:id", (req, res) => {
 });
 
 
+
 /* Aplicação do DELETE, onde o usuário pode deletar qualque objeto que estiver dentro do array */
 app.delete ("/jogador/:id" , (req,res) => {
     const id = parseInt(req.params.id);
@@ -55,7 +70,7 @@ app.delete ("/jogador/:id" , (req,res) => {
 
     if (posicao !== -1) {
         jogador.splice(posicao,1);
-        res.status(200).json({jogadore: jogador, mensagem: "O jogador foi deletado com sucesso!"});
+        res.status(200).json({ mensagem: "O jogador foi deletado com sucesso!"});
     }
     else {
         res.status(404).json({ erro: "Jogador não encontrado."});
@@ -63,20 +78,17 @@ app.delete ("/jogador/:id" , (req,res) => {
 })
 
 
-/* O Array foi criado para guardar as informações do clube*/
-let clube = [
-    { id: 1, time: "Ibis" , presidente: "Ozir Ramos Junior" , estadio: "Estádio Municipal Ademir Cunha"},
-    { id: 2, time: "Ponte preta" , presidente: "Marco Antônio" , estadio: "Estádio Moisés Lucarelli"},
-    { id: 3, time: "Arsenal" , presidente: "Stan Kroenke" , estadio: "Emirates Stadium"},
-    { id: 4, time: "Foraleza" ,  presidente: "José Rolim" , estadio: "Arena Castelão"},
-    { id: 5, time: "Maranhão Atlético clube" , presidente: "Carlos Eduardo" , estadio: "Estádio Governador São Castelo"},
-];
-
+app.post("/jogador" , (req,res) => {
+    const novoJogador = { id: jogador.lenght + 1, ...req.body};
+     jogador.push(novoJogador);
+     res.status(200).json(novoJogador,{ mensagem:"O jogador foi adicionado!"});
+})
 
 /* Aplicação de GET específico por ID do objeto*/
 app.get ("/clube", (req, res) => {                                  
     res.json(clube);
 });
+
 
 
 /* Aplicação do GET geral, revelano o array inteiro */
@@ -91,6 +103,7 @@ app.get ("/clube/:id", (req, res) => {
         res.status(404).json({ erro: "O clube não foi encontrado"});
     }
 })
+
 
 
 /* Aplicação do PUT, onde o usuário pode atualizar alguma informação do array */
@@ -110,17 +123,26 @@ app.put ("/clube/:id", (req,res) => {
 });
 
 
+
 /* Aplicação do DELETE, onde o usuário pode deletar qualque objeto que estiver dentro do array */
 app.delete("/clube/:id" , (req, res) => {
     const id = parseInt(req.params.id);
     const posicao = clube.findIndex(clube => clube.id === id);
 
     if (posicao !== -1) {
-        res.status(200).json({clubes: clube , mensagem: "O clube foi deletado com sucesso!"});
+        res.status(200).json({ mensagem: "O clube foi deletado com sucesso!"});
     }
     else {
         res.status(404).json({ erro: "O clube não foi encontrado"})
     }
+})
+
+
+app.post("clube" , (req,res) => {
+    const novoClube = {id: clube.length + 1, ...req.body};
+    clube.push(novoClube);
+    res.status(200).json({ mensagem:"O clube foi adicionado!"});
+
 })
 
 
